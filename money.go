497f38be4ae76e637579fcea0560337baa22aad4
Jason Larsen <jlarsen@mavenlink.com>
2020-12-17 18:33:42 -0700

    working

--- money.go
+++ money.go
@@ -6,26 +6,30 @@ func main() {
 	fmt.Println("vim-go")
 }
 
-type Dollar struct {
+type Money struct {
 	amount uint64
+}
+
+type Dollar struct {
+	*Money
 }
 
 type Franc struct {
-	amount uint64
+	*Money
 }
 
 func NewDollar(amount uint64) *Dollar {
-	return &Dollar{amount: amount}
+	return &Dollar{Money: &Money{amount: amount}}
 }
 
 func NewFranc(amount uint64) *Franc {
-	return &Franc{amount: amount}
+	return &Franc{Money: &Money{amount: amount}}
 }
 
 func (d *Dollar) Times(multiplier uint64) *Dollar {
-	return &Dollar{amount: d.amount * multiplier}
+	return &Dollar{Money: &Money{amount: d.amount * multiplier}}
 }
 
 func (d *Franc) Times(multiplier uint64) *Franc {
-	return &Franc{amount: d.amount * multiplier}
+	return &Franc{Money: &Money{amount: d.amount * multiplier}}
 }
