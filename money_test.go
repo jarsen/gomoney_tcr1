187a553ac8129a3cf0ae632db7480e24eaffac66
Jason Larsen <jlarsen@mavenlink.com>
2020-12-17 18:14:58 -0700

    working

--- money_test.go
+++ money_test.go
@@ -9,5 +9,5 @@ import (
 func TestMultiplication(t *testing.T) {
 	five := NewDollar(5)
 	assert.Equal(t, five.Times(2), NewDollar(10))
-	assert.Equal(t, *five.Times(3), Dollar{Amount: 15})
+	assert.Equal(t, five.Times(3), NewDollar(15))
 }
