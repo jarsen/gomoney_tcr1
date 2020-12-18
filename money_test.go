cfea6384af9a8ccdd630d4287bf0f0c1a494a144
Jason Larsen <jlarsen@mavenlink.com>
2020-12-17 18:42:09 -0700

    working

--- money_test.go
+++ money_test.go
@@ -7,9 +7,8 @@ import (
 )
 
 func TestDollarMultiplication(t *testing.T) {
-	five := NewDollar(5)
-	assert.Equal(t, five.Times(2), NewDollar(10))
-	assert.Equal(t, five.Times(3), NewDollar(15))
+	assert.Equal(t, NewDollar(5).Times(2), NewDollar(10))
+	assert.Equal(t, NewDollar(5).Times(3), NewDollar(15))
 }
 
 func TestFrancMultiplication(t *testing.T) {
