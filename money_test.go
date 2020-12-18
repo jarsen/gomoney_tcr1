b1228752b264480bcd380b4f43e214877ef25382
Jason Larsen <jlarsen@mavenlink.com>
2020-12-17 18:35:53 -0700

    working

--- money_test.go
+++ money_test.go
@@ -17,3 +17,8 @@ func TestFrancMultiplication(t *testing.T) {
 	assert.Equal(t, five.Times(2), NewFranc(10))
 	assert.Equal(t, five.Times(3), NewFranc(15))
 }
+
+func TestEquality(t *testing.T) {
+	assert.Equal(t, NewDollar(5), NewDollar(5))
+	assert.NotEqual(t, NewDollar(5), NewDollar(6))
+}
