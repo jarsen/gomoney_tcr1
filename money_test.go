11222e97f1f7a5bb72c25f1f7dcaa473d8025dde
Jason Larsen <jlarsen@mavenlink.com>
2020-12-17 18:17:37 -0700

    working

--- money_test.go
+++ money_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMultiplication(t *testing.T) {
+func TestDollarMultiplication(t *testing.T) {
 	five := NewDollar(5)
 	assert.Equal(t, five.Times(2), NewDollar(10))
 	assert.Equal(t, five.Times(3), NewDollar(15))
