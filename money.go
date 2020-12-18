982aef451adceabff4c4e2f54891400deb6af058
Jason Larsen <jlarsen@mavenlink.com>
2020-12-17 18:39:25 -0700

    working

--- money.go
+++ money.go
@@ -30,11 +30,3 @@ func NewFranc(amount uint64) *Money {
 func (m *Money) Times(multiplier uint64) *Money {
 	return &Money{amount: m.amount * multiplier, currency: m.currency}
 }
-
-func (d *Dollar) Times(multiplier uint64) *Dollar {
-	return &Dollar{Money: &Money{amount: d.amount * multiplier}}
-}
-
-func (d *Franc) Times(multiplier uint64) *Franc {
-	return &Franc{Money: &Money{amount: d.amount * multiplier}}
-}
